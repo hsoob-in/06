0f7dd0af01ff9e5898ae6037861431b64d98c411
hsoob-in <tnqls2361@sookmyung.ac.kr>
2025-10-08 15:09:05 +0900

    실습01

--- main.c
+++ main.c
@@ -1,19 +1,19 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-void print_star()
+void print_star(int n)
 {
  	 int i;
- 	 for(i=0; i<10; i++)
+ 	 for(i=0; i<n; i++)
 	  printf("*");
 }
 	 
 
 int main(void)
 {
-  print_star();
-  print_star();
-  print_star();
+  print_star(10);
+  print_star(10);
+  print_star(10);
   
   system("PAUSE");	
   return 0;
