49343c67c4f073974301e7fc15e28596b66b2f69
hsoob-in <tnqls2361@sookmyung.ac.kr>
2025-10-08 15:35:17 +0900

    실습04

--- main.c
+++ main.c
@@ -1,33 +1,16 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-int sumTwo(int a, int b)
+int square(int a)
 {
- 	 
- 	 return(a+b);
-}
-	
-int square(int n)
-{
- 	return(n*n);
+ 	 return(a*a);
 }
 
-int get_max(int x, int y)
+int main()
 {
- 	if (x>y)
- 	 return x;
- 	else
- 	 return y;
-	 
-}
-
-int main(void)
-{
-  
-  printf("sum two result is %i\n", sumTwo(2,3));
-  printf("square result is %i\n", square(5));
-  printf("getmax result is %i\n", get_max(10,13));   
-  
+  int a = 2;
+  a = square(a);
+  printf("a=%i\n", a);
   
   system("PAUSE");	
   return 0;
